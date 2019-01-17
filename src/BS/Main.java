7a8a285e3695546be7723eb8f2631ea57d1b5b6d
allbecome@naver.com <allbecome@naver.com>
2019-01-17 21:12:50 +0900

    3r3

--- src/BS/Main.java
+++ src/BS/Main.java
@@ -26,6 +26,7 @@ public class Main extends JFrame {
 		JButton button = new JButton("Click Me");
 		JButton butto2 = new JButton("ㅗㅗ");
 		JButton butto3 = new JButton("ㅗㅗ3");
+		JButton butto4 = new JButton("ㅗㅗ3");
 		JLabel label = new JLabel("Hello World");
 		add(label);
 		JTextField text = new JTextField(5);
@@ -33,5 +34,6 @@ public class Main extends JFrame {
 		add(button); // then button is visible
 		add(butto2);
 		add(butto3);
+		add(butto4);
 	}
 }
