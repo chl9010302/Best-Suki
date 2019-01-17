131c11f42fccd8ed50c60082388e479777970a94
allbecome@naver.com <allbecome@naver.com>
2019-01-17 20:49:10 +0900

    sung push

--- src/BS/Main.java
+++ src/BS/Main.java
@@ -25,11 +25,13 @@ public class Main extends JFrame {
 		setLayout(new FlowLayout());
 		JButton button = new JButton("Click Me");
 		JButton butto2 = new JButton("ㅗㅗ");
+		JButton butto3 = new JButton("ㅗㅗ3");
 		JLabel label = new JLabel("Hello World");
 		add(label);
 		JTextField text = new JTextField(5);
 		add(text); // the button is visible
 		add(button); // then button is visible
 		add(butto2);
+		add(butto3);
 	}
 }
