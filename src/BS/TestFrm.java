45eba88e749d6c12143051f10a8e0b32b06da87a
chl9010302 <chl901030@gmail.com>
2019-01-18 14:47:42 +0900

    1. Modify MainView - Insert GridLayout

--- src/BS/TestFrm.java
+++ src/BS/TestFrm.java
@@ -1,12 +1,33 @@
 package BS;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class TestFrm extends JFrame{
 	public TestFrm() {
+		JPanel MainView = new JPanel();
 		
+		MainView.setLayout(new BorderLayout());
+		JButton[] bt = new JButton[5];
+		
+		BorderLayout GL1 = new BorderLayout();
+		MainView.setLayout(GL1);
+		
+		for(int i=0; i< bt.length;i++) {
+			bt[i] = new JButton("Button" + i);
+		}
+		MainView.add(bt[0],BorderLayout.NORTH);
+		MainView.add(bt[1],BorderLayout.EAST);
+		MainView.add(bt[2],BorderLayout.CENTER);
+		MainView.add(bt[3],BorderLayout.SOUTH);
+		MainView.add(bt[4],BorderLayout.WEST);
 		setSize(Preference.WIDTH_SIZE, Preference.HEIGHT_SIZE);
 		setLocation(Preference.LOCATION_WIDTH_SIZE, Preference.LOCATION_HEIGHT_SIZE);
+		add(MainView);
 		setVisible(true);
+		
 	}
 }
