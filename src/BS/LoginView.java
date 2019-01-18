2f923784c6e3968abaf5a58862b7dac207258775
chl9010302 <chl901030@gmail.com>
2019-01-18 14:09:46 +0900

    1. Optimized code in JAVA

--- src/BS/LoginView.java
+++ src/BS/LoginView.java
@@ -1,7 +1,6 @@
 package BS;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -20,71 +19,57 @@ public class LoginView extends JFrame{
 	private JTextField userText;
 	private boolean bLoginCheck;
 	
-	public static void main(String[] args) {
-		
-	}
+	public static void main(String[] args) { }
 	
 	public LoginView() {
-		// setting
+		// Login View Setting
 		setTitle(Preference.TITLE_LOGIN);
 		setSize(Preference.WIDTH_SIZE, Preference.HEIGHT_SIZE);
-		setResizable(false);
+		setResizable(true); // LoginView is Resizable
 		setLocation(Preference.LOCATION_WIDTH_SIZE, Preference.LOCATION_HEIGHT_SIZE);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		
-		//panel
-		JPanel panel = new JPanel();
+		setDefaultCloseOperation(EXIT_ON_CLOSE); // LoginView can be closed if CloseOperation is worked
+		JPanel panel = new JPanel(); // create panel
 		placeLoginPanel(panel);
-		
-		// add
-		add(panel);
-		
-		//visible
-		setVisible(true);
+		add(panel); // Add panel ondLoginView
+		setVisible(true); //LoginView is visible
 	}
 	
 	public void placeLoginPanel(JPanel panel){
 		panel.setLayout(null);
 		panel.setBackground(Color.white);
+		//userLabel
 		JLabel userLabel = new JLabel(Preference.USER_ID);
 		userLabel.setBounds(110,210,80,25);
 		panel.add(userLabel);
+		//passLabel
 		JLabel passLabel = new JLabel(Preference.USER_PASSWORD);
 		passLabel.setBounds(110,240,80,25);
 		panel.add(passLabel);
-		
+		//userText
 		userText = new JTextField(20);
 		userText.setBounds(200,210,160,25);
 		panel.add(userText);
-		
+		//passText
 		passText = new JPasswordField(20);
 		passText.setBounds(200,240,160,25);
 		panel.add(passText);
-		passText.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				isLoginCheck();
-			}
-		});
+		//Button of Login
 		btnLogin = new JButton(Preference.TITLE_LOGIN);
 		btnLogin.setBounds(260,280,100,25);
 		panel.add(btnLogin);
-		
+		//Check userID+userPassword
 		btnLogin.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				isLoginCheck();
 			}
 		});
-		
 	}	
-	
 	// login ID와 Password를 체크하여 성공하면 SUCCESS + bLoginCheck, 실패하면 FAIL
 	public void isLoginCheck() {
 		if(userText.getText().equals("test") && new String(passText.getPassword()).equals("1234")) {
 			JOptionPane.showMessageDialog(null, Preference.MESSAGE_SUCCESS);
-			bLoginCheck = true;
+			bLoginCheck = true; // 로그인 됬음을 알려주는 boolean 값
 			
 			// 로그인 성공이라면 매니져창 뛰우기
 			if(isLogin()) {
@@ -97,7 +82,7 @@ public class LoginView extends JFrame{
 	public void setMain(Main main) {
 		this.main = main;
 	}
-	
+
 	public boolean isLogin() {
 		return bLoginCheck;
 	}
