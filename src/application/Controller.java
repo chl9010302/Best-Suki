c9b34bf9c98a279eb0407fd57e66e6fc48c7be00
chl9010302 <chl901030@gmail.com>
2019-01-22 20:13:43 +0900

    Create MainView, SignupView, LoginView

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,21 +1,54 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class Controller {
 	private String operator = "";
 	private int x = 0;
 	private Model model = new Model();
+	Parent myNewScene;
 	
 	@FXML
 	private Text result;
 	
-	@FXML
-	private void operand(ActionEvent event) {
-		result.setText(result.getText() + ((Button) event.getSource()).getText());
+	@FXML // Move LoginView
+	private void NAV_SignUp(ActionEvent event) throws IOException {
+		Parent SignupView = FXMLLoader.load(getClass().getResource("SignupView.fxml"));
+		Scene SignupView_scene = new Scene(SignupView);
+		SignupView_scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		app_stage.setScene(SignupView_scene);
+		app_stage.show();
+	}
+	
+	@FXML // Move LoginView
+	private void NAV_Login(ActionEvent event) throws IOException {
+		Parent LoginView = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+		Scene LoginView_scene = new Scene(LoginView);
+		LoginView_scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		app_stage.setScene(LoginView_scene);
+		app_stage.show();
+	}
+	
+	@FXML // Move LoginView
+	private void NAV_Main(ActionEvent event) throws IOException {
+		Parent MainView = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+		Scene MainView_scene = new Scene(MainView);
+		MainView_scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		app_stage.setScene(MainView_scene);
+		app_stage.show();
 	}
 	
 	@FXML
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,32 +1,28 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	public static String sfxml = "LoginView.fxml";
 	
-	@FXML
-	private String test;
-	
-	@FXML
-	private Button Login;
+	public static void setsfxml(String Sfxml) {
+		sfxml = Sfxml;
+	}
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent fxml = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+			Parent fxml = FXMLLoader.load(getClass().getResource(sfxml));
 			// show root BorderPanel
 			Scene scene = new Scene(fxml);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			System.out.println(test);
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
