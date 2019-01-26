dd7191ccd5772f4dad3a06a50c1c28e0ee60bc17
allbecome@naver.com <allbecome@naver.com>
2019-01-26 13:52:24 +0900

    sung

---
+++ src/DBModel/UserBean.java
@@ -0,0 +1,118 @@
+package DBModel;
+
+public class UserBean { 
+	//s
+	private String UserId            ;
+	private String UserPassword      ;
+	private String UserName          ;
+	private String UserAddress       ;
+	private String UserSchoolName    ;
+	private String UserAge           ;
+	private String UserGender        ;
+	private String UserStudentnumber ;
+	private String UserPhone         ;
+	private String UserFmphone       ;
+	private String UserLoginsession  ;
+	private String Isteacher         ;
+	private String Classnumber       ;
+	private String Classnumber_2     ;
+	private String Classnumber_3     ;
+	private String Classnumber_4     ;
+	public String getUserId() {
+		return UserId;
+	}
+	public void setUserId(String userId) {
+		UserId = userId;
+	}
+	public String getUserPassword() {
+		return UserPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		UserPassword = userPassword;
+	}
+	public String getUserName() {
+		return UserName;
+	}
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	public String getUserAddress() {
+		return UserAddress;
+	}
+	public void setUserAddress(String userAddress) {
+		UserAddress = userAddress;
+	}
+	public String getUserSchoolName() {
+		return UserSchoolName;
+	}
+	public void setUserSchoolName(String userSchoolName) {
+		UserSchoolName = userSchoolName;
+	}
+	public String getUserAge() {
+		return UserAge;
+	}
+	public void setUserAge(String userAge) {
+		UserAge = userAge;
+	}
+	public String getUserGender() {
+		return UserGender;
+	}
+	public void setUserGender(String userGender) {
+		UserGender = userGender;
+	}
+	public String getUserStudentnumber() {
+		return UserStudentnumber;
+	}
+	public void setUserStudentnumber(String userStudentnumber) {
+		UserStudentnumber = userStudentnumber;
+	}
+	public String getUserPhone() {
+		return UserPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		UserPhone = userPhone;
+	}
+	public String getUserFmphone() {
+		return UserFmphone;
+	}
+	public void setUserFmphone(String userFmphone) {
+		UserFmphone = userFmphone;
+	}
+	public String getUserLoginsession() {
+		return UserLoginsession;
+	}
+	public void setUserLoginsession(String userLoginsession) {
+		UserLoginsession = userLoginsession;
+	}
+	public String getIsteacher() {
+		return Isteacher;
+	}
+	public void setIsteacher(String isteacher) {
+		Isteacher = isteacher;
+	}
+	public String getClassnumber() {
+		return Classnumber;
+	}
+	public void setClassnumber(String classnumber) {
+		Classnumber = classnumber;
+	}
+	public String getClassnumber_2() {
+		return Classnumber_2;
+	}
+	public void setClassnumber_2(String classnumber_2) {
+		Classnumber_2 = classnumber_2;
+	}
+	public String getClassnumber_3() {
+		return Classnumber_3;
+	}
+	public void setClassnumber_3(String classnumber_3) {
+		Classnumber_3 = classnumber_3;
+	}
+	public String getClassnumber_4() {
+		return Classnumber_4;
+	}
+	public void setClassnumber_4(String classnumber_4) {
+		Classnumber_4 = classnumber_4;
+	}
+	
+}
