bd985b3ddff3a1b2e227bccdfbb819c3276f152c
chl9010302 <43000146+chl9010302@users.noreply.github.com>
2019-01-29 11:25:40 +0900

    Add files via upload

---
+++ src/DBController/AddBoard.java
@@ -0,0 +1,57 @@
+package DBController;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import DBModel.BoardBean;
+
+public class AddBoard {
+	BoardBean board;
+	Connection conn = null;
+	Statement stmt = null;
+
+	public AddBoard(BoardBean addboard) {
+		insert(addboard);
+	}
+	public boolean insert(BoardBean addboard) {
+		String insertsql1 = "insert into board(BoardId, Subtitle, Filepath, Radio1, Radio2, Radio3, Radio4, Radio5, Flag) values(?, ?, ?, ?, ?, ?, ?, ?, ?);";
+	    PreparedStatement pstmt = null;
+		board = addboard;
+		try {
+			 conn = application.DBConnection.getDBConection();
+			 pstmt = conn.prepareStatement(insertsql1);
+			  			pstmt.setString(1,board.getBoardId() );
+			  			pstmt.setString(2,board.getSubtitle()); 
+			  			pstmt.setString(3, board.getFilepath()); 
+			  			pstmt.setString(4, board.getRadio1()); 
+			  			pstmt.setString(5, board.getRadio2()); 
+			  			pstmt.setString(6, board.getRadio3()); 
+			  			pstmt.setString(7, board.getRadio4()); 
+			  			pstmt.setString(8, board.getRadio5()); 
+			  			pstmt.setString(9, board.getFlag()); 
+			  			pstmt.executeUpdate();
+			conn.close();
+			pstmt.close();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e2) {
+
+			}
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (SQLException e3) {
+
+			}
+		}
+		return false;
+	}
+}
