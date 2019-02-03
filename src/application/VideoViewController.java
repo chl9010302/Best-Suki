911842899c76f6c7e538af1efb8930b1865a6d54
chl9010302 <chl901030@gmail.com>
2019-02-03 16:26:04 +0900

    split controller

--- src/application/VideoViewController.java
+++ src/application/VideoViewController.java
@@ -1,29 +1,11 @@
 package application;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URL;
-import java.security.NoSuchAlgorithmException;
-import java.time.LocalDate;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
-import DBController.AddBoard;
-import DBController.AddStastics;
-import DBController.SelectNowUser;
-import DBController.TestDetailAdd;
-import DBController.UserDataUpdate;
-import DBController.UserJoin;
 import DBController.UserLogin;
-import DBModel.BoardBean;
-import DBModel.TestDetailBean;
-import DBModel.UserBean;
-import ImageStore.TestImageStore;
-import academyutil.Sha256;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -33,19 +15,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.text.Text;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class VideoViewController implements Initializable {
